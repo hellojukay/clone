40afaae8d4a90923e61c9f697c67ee31621ca8dd
hellojukay <licong@qianxin.com>
2020-12-22 12:26:52 +0800

    complete

--- main.go
+++ main.go
@@ -2,18 +2,39 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
 )
 
 var repo string
 
 func init() {
-	flag.StringVar(&repo, "repo", "", "github repostiry https URL")
+	flag.StringVar(&repo, "repo", "", "github repository https URL")
 	flag.Parse()
 	if repo == "" {
 		flag.PrintDefaults()
 	}
-
+	// check repo url
+	if strings.HasPrefix(repo, "git@") {
+		fmt.Fprintf(os.Stderr, "%s\n", "repository ssh URL not support noew, please use https URl")
+		os.Exit(1)
+	}
+	if !strings.HasPrefix(repo, "https://github.com") {
+		fmt.Fprintf(os.Stderr, "%s\n", "only support github repository now")
+		os.Exit(1)
+	}
+	repo = strings.Replace(repo, "github.com", "hub.fastgit.org", 1)
 }
-func main() {
 
+func main() {
+	var clone = fmt.Sprintf("git clone %s", repo)
+	fmt.Printf("%s\n", clone)
+	var c = exec.Command("git", "clone", repo)
+	err := c.Run()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "run git clone failed %s", err)
+		os.Exit(1)
+	}
 }
